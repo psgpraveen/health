60ccd4dbf04fc15bf34743364c9aa5bd63951406
Praveen Kumar Gupta <pg23403@gmail.com>
2025-04-20 10:11:29 +0530

    Update

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,15 @@
-// types/next-auth.d.ts
-import NextAuth from "next-auth";
+// src/types/next-auth.d.ts
+
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
+    user: DefaultSession["user"] & {
       role: "doctor" | "patient";
     };
+  }
+
+  interface User extends DefaultUser {
+    role: "doctor" | "patient";
   }
 }
